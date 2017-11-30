b2913f1556767db685cd0914e8ca75a570b0a691
tongyuehong1 <245503112@qq.com>
2017-11-30 21:09:35 +0800

    range练习

---
+++ range.go
@@ -0,0 +1,20 @@
+package main
+import "fmt"
+
+func main() {
+	num := []int{2, 3, 4}
+	sum := 0
+	for _,num := range num {
+		sum += num
+	}
+	fmt.Printf("sum: %d\n", sum)
+	for i, num := range num {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+	for i := range num {
+		num[i] = num[i] << 2
+	}
+	fmt.Println("输出是", num)
+}
