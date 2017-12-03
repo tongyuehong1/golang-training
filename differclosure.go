5ed7e33f958fb96e744bd987a68734f0ccfe51c3
tongyuehong1 <245503112@qq.com>
2017-12-03 20:01:01 +0800

    闭包

---
+++ differclosure.go
@@ -0,0 +1,26 @@
+package main
+import "fmt"
+
+func main() {
+	// 普通函数调用, 输出结果为1，2，3，（defer后）3，2，1
+	var i int
+	a := []int{1, 2, 3}
+	for _, i = range a{
+		fmt.Println(i)
+		defer some(i)
+	}
+
+	// 闭包, 输出结果是4，5，6，（defer后）6，6，6
+	var m int
+	b := []int{4, 5, 6}
+	for _, m = range b{
+		fmt.Println(m)
+		defer func(){
+			fmt.Println(m)
+		}()
+	}
+}
+func some(i int) {
+	fmt.Println(i)
+}
+
