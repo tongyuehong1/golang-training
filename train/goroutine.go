5b4c37f531c1b2f4e9d5df0cbe48350e73a370d8
tongyuehong1 <245503112@qq.com>
2017-12-05 21:01:27 +0800

    channel与routine

---
+++ train/goroutine.go
@@ -0,0 +1,38 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("主函数")
+
+	go func() {
+		fmt.Println("停顿五秒")
+		time.Sleep(time.Second*5)
+	}()
+
+	go func() {
+		fmt.Println("停顿两秒")
+		time.Sleep(time.Second*2)
+	}()
+
+	ch := make(chan int)
+	go send(ch)
+	go get(ch)
+	
+	time.Sleep(time.Second*1)
+}
+
+func send(ch chan int) {
+	ch <- 1
+	ch <- 2
+	ch <- 3
+}
+func get(ch chan int) {
+	var num int
+	for {
+		num =<- ch
+		fmt.Println("num:", num)
+	}
+}
